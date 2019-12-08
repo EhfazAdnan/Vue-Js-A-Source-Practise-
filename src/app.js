f914dc5a97eb3320cd1109abc874c0206ad25e97
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-08 13:38:59 +0600

    Call a api and get data with axios and show with li

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Accordion from './components/Accordion';
 import List from './components/List';
 
+window.axios = require('axios');
+
 new Vue({
    el: '#app',
 
@@ -9,13 +11,22 @@ new Vue({
       Accordion,
       List,
    },
+
+   mounted: function(){
+       axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => this.posts = response.data)
+            .catch(error => this.posts = [{title: 'No posts found.'}])
+            .finally(() => console.log('Data loading complete'));
+   },
    
    data: {
        items: [
            { id: 1, title: 'Title 1', description: 'Description for tab 1.'},
            { id: 2, title: 'Title 2', description: 'Description for tab 2.'},
            { id: 3, title: 'Title 3', description: 'Description for tab 3.'},
-       ]
+       ],
+
+       posts: null,
    }
 
 });
